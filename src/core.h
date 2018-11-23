16c24b1f1757b9f9f7e18bb0e5ec5f971e5c9f48
Nash Bean <iBeanSoftware@gmail.com>
2018-11-23 15:19:48 +0000

    Create core.h
    The start of something new.

---
+++ src/core.h
@@ -0,0 +1,16 @@
+#include <asp/asp_config.h>
+
+#ifndef __ASP_CORE_H__
+#define __ASP_CORE_H__
+// https://github.com/NashBean/StereoPipeline/src/asp/Core/BlobIndexThreaded.h
+#include <../../StereoPipeline/src/asp/Core/BlobIndexThreaded.h>
+#include <../../StereoPipeline/src/asp/Core/BundleAdjustUtils.h>
+#include <../../StereoPipeline/src/asp/Core/ControlNetworkLoader.h>
+#include <../../StereoPipeline/src/asp/Core/InpaintView.h>
+#include <../../StereoPipeline/src/asp/Core/OrthoRasterizer.h>
+#include <../../StereoPipeline/src/asp/Core/SoftwareRenderer.h>
+#include <../../StereoPipeline/src/asp/Core/SparseView.h>
+#include <../../StereoPipeline/src/asp/Core/StereoSettings.h>
+#include <../../StereoPipeline/src/asp/Core/Macros.h>
+
+#endif//__ASP_CORE_H__
