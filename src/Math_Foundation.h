6ad97ef328078ca4c7ceac0170b292f844346c04
Nash Bean <iBeanSoftware@gmail.com>
2018-12-20 13:39:38 -0500

    Create Math_Foundation.h

---
+++ src/Math_Foundation.h
@@ -0,0 +1,29 @@
+//
+//  Math_Foundation.h
+//  Get cureent version off Github:
+//  https://github.com/NashBean/NashOS/blob/master/Math_Foundation.h
+//
+//  Created by nash(https://github.com/NashBean) on December 20, 2018.
+//  Copyright 2018, 2019 iBean Software(https://iBeanSoftware.github.io),
+//  All rights reserved.
+//   iBeanSoftware@GMail.com
+//-------------------------------------------------------------------------
+//  Will be core math stucts and functions for NashOS
+//-------------------------------------------------------------------------
+
+#ifndef iBS_Math_Foundation_h
+#define iBS_Math_Foundation_h
+
+#include <vector>
+//#include <sstream>
+//#include <fstream>
+
+const int Math_Foundation_MAJOR_VERSION = 0;
+const int Math_Foundation_MINOR_VERSION = 1;
+
+namespace iBS 
+{
+  #define digit uint8_t
+
+}
+#endif // iBS_Math_Foundation_h
